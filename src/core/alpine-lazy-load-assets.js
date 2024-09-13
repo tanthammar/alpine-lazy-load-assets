109dfc4815613601327c2c42a95b707490380ecd
Tina Hammar <tinahammar@gmail.com>
2024-09-13 16:41:07 +0200

    switch functions to const

--- src/core/alpine-lazy-load-assets.js
+++ src/core/alpine-lazy-load-assets.js
@@ -13,111 +13,106 @@ export default function (Alpine) {
         }
     })
 
-    //replicate Alpine.dispatch
-    function assetLoadedEvent(eventName) {
-        return new CustomEvent(eventName, {
-            bubbles: true,
-            composed: true,
-            cancelable: true,
-        })
-    }
+    // Replicate Alpine.dispatch
+    const assetLoadedEvent = eventName => new CustomEvent(eventName, {
+        bubbles: true,
+        composed: true,
+        cancelable: true,
+    })
 
-    function createDomElement(elementType, attributes = {}, targetElement, insertBeforeElement) {
-        const element = document.createElement(elementType);
-
-        for (const [attribute, value] of Object.entries(attributes)) {
-            element[attribute] = value;
-        }
+    const createDomElement = (elementType, attributes = {}, targetElement, insertBeforeElement) => {
+        const element = document.createElement(elementType)
+        Object.entries(attributes).forEach(([attribute, value]) => element[attribute] = value)
 
         if (targetElement) {
             if (insertBeforeElement) {
-                targetElement.insertBefore(element, insertBeforeElement);
+                targetElement.insertBefore(element, insertBeforeElement)
             } else {
-                targetElement.appendChild(element);
+                targetElement.appendChild(element)
             }
         }
 
-        return element;
+        return element
     }
 
-    function loadAsset(elementType, path, attributes = {}, targetElement = null, insertBeforeElement = null) {
-        const selector = (elementType === 'link') ? `link[href="${path}"]` : `script[src="${path}"]`;
+    const loadAsset = (elementType, path, attributes = {}, targetElement = null, insertBeforeElement = null) => {
+        const selector = elementType === 'link' ? `link[href="${path}"]` : `script[src="${path}"]`
 
         if (document.querySelector(selector) || Alpine.store('lazyLoadedAssets').check(path)) {
             return Promise.resolve()
         }
 
-        const elementAttributes = elementType === "link" ? { ...attributes, href: path } : { ...attributes, src: path }
-        const element = createDomElement(elementType, elementAttributes, targetElement, insertBeforeElement);
+        const elementAttributes = elementType === 'link' ? { ...attributes, href: path } : { ...attributes, src: path }
+        const element = createDomElement(elementType, elementAttributes, targetElement, insertBeforeElement)
 
         return new Promise((resolve, reject) => {
             element.onload = () => {
-                Alpine.store('lazyLoadedAssets').markLoaded(path);
-                resolve();
+                Alpine.store('lazyLoadedAssets').markLoaded(path)
+                resolve()
             }
 
             element.onerror = () => {
-                reject(new Error(`Failed to load ${elementType}: ${path}`));
+                reject(new Error(`Failed to load ${elementType}: ${path}`))
             }
-        });
+        })
     }
 
-    async function loadCSS(path, mediaAttr, position = null, target = null) {
+    const loadCSS = async (path, mediaAttr, position = null, target = null) => {
         // Define attributes for CSS link element
-        const attributes = { type: 'text/css', rel: 'stylesheet' };
+        const attributes = { type: 'text/css', rel: 'stylesheet' }
         if (mediaAttr) {
-            attributes.media = mediaAttr;
+            attributes.media = mediaAttr
         }
 
-        // Determine target element and position
-        let targetElement = document.head;
-        let insertBeforeElement = null;
+        let targetElement = document.head
+        let insertBeforeElement = null
 
+        // Determine target element and position
         if (position && target) {
 
             // A certain link element has been targeted
             const targetLink = document.querySelector(`link[href*="${target}"]`)
             if (targetLink) {
-                targetElement = targetLink.parentElement; // Ensure we use the parent element for insertion
-                insertBeforeElement = (position === 'before') ? targetLink : targetLink.nextSibling;
+                targetElement = targetLink.parentElement // Ensure we use the parent element for insertion
+                insertBeforeElement = position === 'before' ? targetLink : targetLink.nextSibling
             } else {
-                console.warn(`Target (${target}) not found for ${path}. Appending to head.`);
-                targetElement = document.head;
-                insertBeforeElement = null;
+                console.warn(`Target (${target}) not found for ${path}. Appending to head.`)
+                targetElement = document.head
+                insertBeforeElement = null
             }
         }
 
-        await loadAsset('link', path, attributes, targetElement, insertBeforeElement);
+        await loadAsset('link', path, attributes, targetElement, insertBeforeElement)
     }
 
-    async function loadJS(path, position, relativePosition = null, targetScript = null) {
+    const loadJS = async (path, position, relativePosition = null, targetScript = null) => {
         // Default insertion point is head
-        let targetElement = document.head;
-        let insertBeforeElement = null;
+        let targetElement = document.head
+        let insertBeforeElement = null
 
         if (relativePosition && targetScript) {
 
             //A certain script element has been targeted
-            const targetScriptElement = document.querySelector(`script[src*="${targetScript}"]`);
+            const targetScriptElement = document.querySelector(`script[src*="${targetScript}"]`)
             if (targetScriptElement) {
-                targetElement = targetScriptElement.parentElement; // Ensure we use the parent element for insertion
-                insertBeforeElement = relativePosition === 'before' ? targetScriptElement : targetScriptElement.nextSibling;
+                targetElement = targetScriptElement.parentElement // Ensure we use the parent element for insertion
+                insertBeforeElement = relativePosition === 'before' ? targetScriptElement : targetScriptElement.nextSibling
             } else {
-                console.warn(`Target (${targetScript}) not found for ${path}. Falling back to head or body.`);
-                targetElement = document.head;
-                insertBeforeElement = null;
+                console.warn(`Target (${targetScript}) not found for ${path}. Falling back to head or body.`)
+                targetElement = document.head
+                insertBeforeElement = null
             }
 
         } else if (position.has('body-start') || position.has('body-end')) {
 
             //the js should go into body
-            targetElement = document.body;
-            if(position.has('body-start')) {
-                insertBeforeElement = document.body.firstChild;
+            targetElement = document.body
+            if (position.has('body-start')) {
+                insertBeforeElement = document.body.firstChild
             }
         }
 
-        await loadAsset('script', path, {}, targetElement, insertBeforeElement);
+        await loadAsset('script', path, {}, targetElement, insertBeforeElement)
     }
 
     Alpine.directive('load-css', (el, { expression }, { evaluate }) => {
@@ -132,12 +127,10 @@ export default function (Alpine) {
                 //console.log('All CSS files loaded!')
                 if (eventName) {
                     //console.log('dispatching event: ' + eventName + '-css')
-                    window.dispatchEvent(assetLoadedEvent(eventName + '-css'))
+                    window.dispatchEvent(assetLoadedEvent(`${eventName}-css`))
                 }
             })
-            .catch((error) => {
-                console.error(error)
-            })
+            .catch(console.error)
     })
 
     Alpine.directive('load-js', (el, { expression, modifiers }, { evaluate }) => {
@@ -145,7 +138,6 @@ export default function (Alpine) {
         const position = new Set(modifiers)
         const relativePosition = el.getAttribute('data-js-before') ? 'before' : el.getAttribute('data-js-after') ? 'after' : null
         const targetScript = el.getAttribute('data-js-before') || el.getAttribute('data-js-after') || null
-
         const eventName = el.getAttribute('data-dispatch')
 
         Promise.all(paths.map(path => loadJS(path, position, relativePosition, targetScript)))
@@ -153,11 +145,9 @@ export default function (Alpine) {
                 //console.log('All JS files loaded!')
                 if (eventName) {
                     //console.log('dispatching event: ' + eventName + '-js')
-                    window.dispatchEvent(assetLoadedEvent(eventName + '-js'))
+                    window.dispatchEvent(assetLoadedEvent(`${eventName}-js`))
                 }
             })
-            .catch((error) => {
-                console.error(error)
-            })
+            .catch(console.error)
     })
 }
